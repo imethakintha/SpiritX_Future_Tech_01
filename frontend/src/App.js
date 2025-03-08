b40786af91192b3c9dbba1c4544ca593896b4fa6
imethakintha <imethgithubofficial@gmail.com>
2025-03-08 13:08:00 +0530

    Add initial frontend setup with React, routing, and basic styles with Signup page

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Landing from './components/Landing';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/landing" element={<Landing />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
